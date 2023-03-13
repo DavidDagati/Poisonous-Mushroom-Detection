49766d1add344ecf0b973d430ee31681fc449a09
David Dagati <ddagati@hotmail.ca>
2023-03-13 13:14:14 -0400

    first commit

---
+++ analysis.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def convert_to_full(data):
+    # This will convert the single letters to full words for an easier read
+
+    full_dict = {
+
+    }
+
+    
+    return full_data
+
+if __name__ == '__main__':
+    
+    data = pd.read_csv('mush_data.csv')
+    print(data.head())
